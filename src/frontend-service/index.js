143c69c01b022a13d308320e7dfda6274fa67c0a
Diego <d.lazaro@sabienzia.com>
2020-02-26 16:26:48 +0100

    try front build

--- src/frontend-service/index.js
+++ src/frontend-service/index.js
@@ -1,13 +1,14 @@
-var express = require('express');
+var express = require("express");
+
 var app = express();
-var path = require('path');
+var path = require("path");
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get('/results', function(req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'results.html'));
+app.get("/results", function(req, res) {
+  res.sendFile(path.join(__dirname, "public", "results.html"));
 });
 
-app.listen(8080, () => console.log('Nodejs Frontend listening on port 8080!'))
+app.listen(8080, () => console.log("Nodejs Frontend listening on port 8080!"));
